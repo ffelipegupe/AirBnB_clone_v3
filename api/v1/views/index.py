afba7281f130ef2697dd60123b610cddf81843e2
ffelipegupe <1560@holbertonschool.com>
2020-09-09 23:18:23 +0000

    Feature: stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,27 @@
 """ Index module """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
+statsDict = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
 
 
 @app_views.route('/status', strict_slashes=False)
 def ok_status():
     """ API status """
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats', strict_slashes=False)
+def obj_stats():
+    """ Endpoint that retrieves the number of each objects by type """
+    dict_stats = {}
+    for key, value in statsDict.items():
+        dict_stats[key] = storage.count(value)
+    return jsonify(dict_stats)
