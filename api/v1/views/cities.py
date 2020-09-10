0a4935fd3b8da2b9ab41c70440f54dcaf9ece2a7
ffelipegupe <1560@holbertonschool.com>
2020-09-10 22:10:15 +0000

    Update: adding methods

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -18,7 +18,7 @@ def get_city_state(state_id):
 
 
 @app_views.route('/cities/<city_id>',
-    methods=['GET'], strict_slashes=False)
+                 methods=['GET'], strict_slashes=False)
 def get_city(city_id):
     """ Retrieves a City object """
     city = storage.get('City', city_id)
