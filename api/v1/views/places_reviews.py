861c21270c2c1e4342a5f430ffdb15f28c9ba3fd
ffelipegupe <1560@holbertonschool.com>
2020-09-11 03:34:06 +0000

    Edit: updating

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-''' Places API actions'''
+""" Review API actions """
 from api.v1.views import app_views
 from models import storage
 from models.place import Place
-from models.city import City
+from models.user import User
 from models.review import Review
 from flask import Flask, jsonify, make_response, request, abort
 
